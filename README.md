cca50615e5a37aab76c923e9be24cec8f5a6a48a
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-17 19:03:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Mobile application testing - Proto Mail <br>
+
+### Deploy
+#### $ npm install
+
+## Usage
+Run tests:
+#### $ npm test
---
+++ test/specs/spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+describe('Simple test', () => {
+    // beforeAll(() => {
+    //     browser.get(baseUri);
+    // });
+
+    it('should have correct title', () => {
+        console.log('App test works!!!')
+        browser.debug();
+    });
+
+
+    // afterAll(() => {
+    //     browser.driver.quit();
+    // });
+});
